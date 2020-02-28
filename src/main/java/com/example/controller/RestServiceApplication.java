80eeb0504561b8debd92265e63e87706c5ef3d57
Carlos <carlos.lopez@kairosds.com>
2020-02-28 10:07:11 -0600

    su seben cambios para construir con test maven

--- src/main/java/com/example/controller/RestServiceApplication.java
+++ src/main/java/com/example/controller/RestServiceApplication.java
@@ -1,7 +1,9 @@
 package com.example.controller;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class RestServiceApplication {
--- src/test/java/com/example/curso/RestServiceApplicationTests.java
+++ src/test/java/com/example/curso/RestServiceApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.curso;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootConfiguration
 class RestServiceApplicationTests {
+	
+	public Logger log = LoggerFactory.getLogger(RestServiceApplicationTests.class);
 
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void testHello() {
+		log.info("into method testHello");
+	}
+
 
 }
